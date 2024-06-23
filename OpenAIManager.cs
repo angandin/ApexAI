9ef846abc96cb97416f6cc7b6298dc9d578ab9f7
Andrea Gandini <angandin@microsoft.com>
2024-06-23 21:51:36 +0200

    change pictures

--- OpenAIManager.cs
+++ OpenAIManager.cs
@@ -31,21 +31,21 @@ namespace ApexAI
             _kernel = builder.Build();
         }
 
-        public async Task<string> GetCompletion(string prompt)
-        {
-            var response = await _kernel.InvokePromptAsync(prompt);
-            //Console.WriteLine(response);
-            return response.ToString();
-        }
+        //public async Task<string> GetCompletion(string prompt)
+        //{
+        //    var response = await _kernel.InvokePromptAsync(prompt);
+        //    //Console.WriteLine(response);
+        //    return response.ToString();
+        //}
 
-        public async Task<string> GetCompletionV2(string prompt)
+        public async Task<string> GetCompletionV2(string prompt, string imagePath)
         {
             var chat = _kernel.GetRequiredService<IChatCompletionService>();
             var history = new ChatHistory();
 
-            history.AddSystemMessage("You are a useful assistant that replies using a direct style");
+            history.AddSystemMessage("You are an AI assistant that helps workstation manager, in a manufacturing plant, to identify from a picture all security risks and if all tools and objects are at their place.\r\nAs input you will receive a picture and a set of tools each that must be appear in a specific place, and all security compliance to check.\r\nAs output, answer with only a json with values based on the check to be performed.");
             var imageContent = new ImageContent();
-            imageContent.Uri = new Uri("https://raw.githubusercontent.com/elbruno/gpt4o-labs-csharp/main/src/GPT4o_AOAI_lab01/imgs/foggyday.png");
+            imageContent.Uri = new Uri($"https://github.com/angandin/ApexAI/blob/master/wwwroot/{imagePath}?raw=true");
 
             var collectionItems = new ChatMessageContentItemCollection
             {
